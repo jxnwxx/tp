762007c4ce5470322ea1890eb5da3bd562ea9c87
jxnwxx <49605139+jxnwxx@users.noreply.github.com>
2025-10-13 22:00:37 +0800

    Implement ListAppointment functionality

---
+++ src/main/java/seedu/address/logic/parser/ListAppointmentCommandParser.java
@@ -0,0 +1,48 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT_DATETIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT_TITLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.ListAppointmentCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.appointment.Title;
+import seedu.address.model.person.Nric;
+
+/**
+ * Parses input arguments and creates a new AddAppointmentCommand object
+ */
+public class ListAppointmentCommandParser implements Parser<ListAppointmentCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddAppointmentCommand
+     * and returns an AddAppointmentCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ListAppointmentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NRIC);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NRIC) || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(
+                    MESSAGE_INVALID_COMMAND_FORMAT, ListAppointmentCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NRIC);
+
+        Nric nric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_NRIC).get());
+
+        return new ListAppointmentCommand(nric.toString());
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
