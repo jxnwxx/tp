562f4eb93f53cc3729ac2e1a145d009071d4b1e0
Timothy Quek <36731402+WhyisQuek@users.noreply.github.com>
2025-10-23 10:24:48 +0800

    Remove incomplete test

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -100,11 +100,6 @@ public class AddressBookParserTest {
         assertEquals(new AddAppointmentCommand(INDEX_FIRST_PATIENT, appointment), command);
     }
 
-    @Test void parseCommand_deleteAppointment() throws Exception {
-        Patient patient = new PatientBuilder().build();
-
-    }
-
     @Test
     public void parseCommand_unrecognisedInput_throwsParseException() {
         assertThrows(ParseException.class, String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE), ()
