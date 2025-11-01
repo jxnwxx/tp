02e9595779cd06f3bd4740b9d834b41b4f62a704
Ruinim <89561179+Ruinim@users.noreply.github.com>
2025-11-01 17:40:13 +0800

    Fix error message of edit-appt and delete-appt to make sense

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -22,7 +22,8 @@ public class Messages {
                 "Multiple values specified for the following single-valued field(s): ";
     public static final String MESSAGE_NOT_VIEWING_PATIENT_LIST = "Command only works when displaying patients.\n"
             + "Use the following command first: list";
-    public static final String MESSAGE_NOT_VIEWING_APPOINTMENT = "Command only works when displaying appointments.\n"
+    public static final String MESSAGE_NOT_VIEWING_APPOINTMENT = "Command only works when "
+            + "displaying a patients' appointments.\n"
             + "Use the following command first: list-appt [INDEX]";
 
     /**
